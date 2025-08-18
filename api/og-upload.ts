8ff48eefc0eb842c8c3c53b15d770c75851e4782
DEVMO <mohamedwael2001193@gmail.com>
2025-08-18 09:50:15 -0400

    Fix SyntaxError in api/og-upload.ts

--- api/og-upload.ts
+++ api/og-upload.ts
@@ -55,7 +55,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
         bb.on("error", reject);
         bb.on("finish", () => {
-          if (!wrote) reject(new Error(\'No file provided (expected field name "file")\'));
+          if (!wrote) reject(new Error('No file provided (expected field name "file")'));
         });
 
         (req as any).pipe(bb);
