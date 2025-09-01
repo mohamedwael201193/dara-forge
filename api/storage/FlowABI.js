9e7b1f4ec42e2ffcb4f8e2a39585ad4114a9fb97
Mohamed Wael <mohamedwael201193@gmail.com>
2025-09-01 14:15:10 -0400

    fix(vercel): Compile FlowABI.ts to FlowABI.js and update import path

---
+++ api/storage/FlowABI.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FLOW_ABI = void 0;
+exports.FLOW_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "_merkleRoot",
+                "type": "bytes32"
+            }
+        ],
+        "name": "submit",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
